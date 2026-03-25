0442094c44cc6ffac8d0f6e389a6d5ea01b5aecf
Ainoa Dura Bordonado <ainoadura@outlook.com>
2026-03-25 19:01:06 +0100

    .

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());          
 app.use(express.json());  
 
-app.use('/api/v1/tasks', taskRoutes);
+app.use('/', taskRoutes);
 
 app.use((err, req, res, next) => {
     console.error('--- DETALLE TÉCNICO DEL ERROR ---');
